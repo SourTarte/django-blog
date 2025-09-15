a0169b8f85c4d789aa8911674a9726cb433005d5
SourTarte <dioncmcmillan@gmail.com>
2025-09-15 14:24:52 +0100

    Start of Templates and Homepage

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Post) #This decorator is used for registering classes, unlike how we registered the models below
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title']
+    list_filter = ('status', 'created_on',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)
